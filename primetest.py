25b8fbd1d48fc2177e07ebcef59b9a307b52a8dd
Terminal <124107188+TerminalX2@users.noreply.github.com>
2023-12-16 18:19:50 -0800

    Update primetest.py

--- primetest.py
+++ primetest.py
@@ -7,6 +7,7 @@ def division(p: int) -> bool:
     return True
   elif p % 2 == 0 or p % 3 == 0:
     return False
+    
   limit = int(p**0.5) + 1
   for i in range(5, limit, 6):
     if p % i == 0 or p % (i + 2) == 0:
